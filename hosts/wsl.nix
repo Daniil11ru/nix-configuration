f21572613ac319eb5ec9b066418b883f6c162281
Daniil Miroshnik <daniilmiroshnik11062001@gmail.com>
2025-11-08 23:02:54 +0300

    feat: integrate bash history to zsh

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -8,12 +8,33 @@
   time.timeZone = "Europe/Moscow";
 
   programs.bash = {
+    interactiveShellInit = ''
+      shopt -s histappend
+      pc_snippet='history -a; history -r'
+      if [[ $PROMPT_COMMAND != *"$pc_snippet"* ]]; then
+        if [[ -n "$PROMPT_COMMAND" ]]; then
+          PROMPT_COMMAND="$pc_snippet; $PROMPT_COMMAND"
+        else
+          PROMPT_COMMAND="$pc_snippet"
+        fi
+      fi
+    '';
+
     blesh.enable = true;
   };
   environment.pathsToLink = [ "/share/bash-completion" ];
 
   programs.zsh = {
     enable = true;
+
+    interactiveShellInit = ''
+      if [[ -r ~/.bash_history ]] && (( ! ''${+_BASH_HISTORY_IMPORTED} )); then
+        fc -RI ~/.bash_history 2>/dev/null || true
+        typeset -g _BASH_HISTORY_IMPORTED=1
+      fi
+
+      setopt INC_APPEND_HISTORY EXTENDED_HISTORY
+    '';
 
     enableCompletion = true;
     enableBashCompletion = true;
